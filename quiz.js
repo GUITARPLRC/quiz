be5ad18eec018005e7dec67b0264a1d9d97f51f8
Charles <GUITARPLRC@YAHOO.COM>
2016-03-06 17:08:35 -0600

    started adding questions

--- quiz.js
+++ quiz.js
@@ -2,82 +2,82 @@ document.onload(function(){
 	
 	var easyQuestions = [
 		{
-			question:,
-			choices:[],
-			correctAnswer:
+			question:"What is the first name of the tennis playing sister of Serena Williams?",
+			choices:["Khloe","Venus","Amy","Vanessa"],
+			correctAnswer:1
 		},
 		{
-			question:,
-			choices:[],
-			correctAnswer:
+			question:"A variety of apple called Granny Smith, is what color?",
+			choices:["blue","orange","red","green"],
+			correctAnswer:3
 		},
 		{
-			question:,
-			choices:[],
-			correctAnswer:
+			question:"What is the name of the kids show that features big purple dinosaur?",
+			choices:["Thomas","Fred","Barney","The Wiggles"],
+			correctAnswer:2
 		},
 		{
-			question:,
-			choices:[],
-			correctAnswer:
+			question:"Who is condisered to be the greatest basketball player of all time?",
+			choices:["Steph Curry","Michael Jordan","Larry Bird","Kobe Bryant"],
+			correctAnswer:1
 		},
 		{
-			question:,
-			choices:[],
+			question:"What is the capital of the United States of America?",
+			choices:["Washington, D.C.","Seattle","Maryland","Canada"],
 			correctAnswer:
 		}];
 	
 	var mediumQuestions = [
 		{
-			question:,
-			choices:[],
+			question:"What is James Bond's secret agent number?",
+			choices:["911","80","4","007"],
+			correctAnswer:3
+		},
+		{
+			question:"How many edges does a cube have?",
+			choices:["6","1","5","10"],
+			correctAnswer:0
+		},
+		{
+			question:"",
+			choices:["","","",""],
 			correctAnswer:
 		},
 		{
-			question:,
-			choices:[],
+			question:"",
+			choices:["","","",""],
 			correctAnswer:
 		},
 		{
-			question:,
-			choices:[],
-			correctAnswer:
-		},
-		{
-			question:,
-			choices:[],
-			correctAnswer:
-		},
-		{
-			question:,
-			choices:[],
+			question:"",
+			choices:["","","",""],
 			correctAnswer:
 		}];
 	
 	var hardQuestions = [
 		{
-			question:,
-			choices:[],
+			question:"",
+			choices:["","","",""],
 			correctAnswer:
 		},
 		{
-			question:,
-			choices:[],
+			question:"",
+			choices:["","","",""],
 			correctAnswer:
 		},
 		{
-			question:,
-			choices:[],
+			question:"",
+			choices:["","","",""],
 			correctAnswer:
 		},
 		{
-			question:,
-			choices:[],
+			question:"",
+			choices:["","","",""],
 			correctAnswer:
 		},
 		{
-			question:,
-			choices:[],
+			question:"",
+			choices:["","","",""],
 			correctAnswer:
 		}];
 })
