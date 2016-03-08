af1dcea7a96925f7263c977ed7a9f090a37a6869
Charles <GUITARPLRC@YAHOO.COM>
2016-03-07 19:11:06 -0600

    adding functions

--- quiz.js
+++ quiz.js
@@ -1,86 +1,108 @@
-document.onload(function(){
-	
-	var easyQuestions = [
-		{
-			question:"What is the first name of the tennis playing sister of Serena Williams?",
-			choices:["Khloe","Venus","Amy","Vanessa"],
-			correctAnswer:1
-		},
-		{
-			question:"A variety of apple called Granny Smith, is what color?",
-			choices:["blue","orange","red","green"],
-			correctAnswer:3
-		},
-		{
-			question:"What is the name of the kids show that features big purple dinosaur?",
-			choices:["Thomas","Fred","Barney","The Wiggles"],
-			correctAnswer:2
-		},
-		{
-			question:"Who is condisered to be the greatest basketball player of all time?",
-			choices:["Steph Curry","Michael Jordan","Larry Bird","Kobe Bryant"],
-			correctAnswer:1
-		},
-		{
-			question:"What is the capital of the United States of America?",
-			choices:["Washington, D.C.","Seattle","Maryland","Canada"],
-			correctAnswer:0
-		}];
-	
-	var mediumQuestions = [
-		{
-			question:"What is James Bond's secret agent number?",
-			choices:["911","80","4","007"],
-			correctAnswer:3
-		},
-		{
-			question:"How many edges does a cube have?",
-			choices:["6","1","5","10"],
-			correctAnswer:0
-		},
-		{
-			question:"What is the biggest continent in the world?",
-			choices:["Europe","Asia","Canada","Alaska"],
-			correctAnswer:1
-		},
-		{
-			question:"Which comic book superhero does not have any 'special powers'?",
-			choices:["Superman","Flash","Batman","Pinocchio"],
-			correctAnswer:2
-		},
-		{
-			question:"Who is usually credited for the invention of the light bulb?",
-			choices:["Thomas Edison","Albert Einstein","Geroge Washington","Isaac Newton"],
-			correctAnswer:0
-		}];
-	
-	var hardQuestions = [
-		{
-			question:"What does the 'www' stand for in 'www.google.com'?",
-			choices:["webpage","world wide web","whiskey while working","internet"],
-			correctAnswer:1
-		},
-		{
-			question:"What is the square root of the number 25?",
-			choices:["1","7.43","625","5"],
-			correctAnswer:3
-		},
-		{
-			question:"What American icon wore a 'coonskin cap?",
-			choices:["Henry Ford","Paul Bunyan","Davy Crockett","John Adams"],
-			correctAnswer:2
-		},
-		{
-			question:"What kind of car did the 'Dukes of Hazard' drive",
-			choices:["Dodge Charger","Dodge Neon","Chevrolet Camaro","Ford Thunderbird"],
-			correctAnswer:0
-		},
-		{
-			question:"What kind of animal was Lassie",
-			choices:["elephant","horse","dolphin","dog"],
-			correctAnswer:3
-		}];
-		
-		var diff = 0; //difficulty
+document.onload(function() {
 
+  var easyQuestions = [{
+    question: "What is the first name of the tennis playing sister of Serena Williams?",
+    choices: ["Khloe", "Venus", "Amy", "Vanessa"],
+    correctAnswer: 1
+  }, {
+    question: "A variety of apple called Granny Smith, is what color?",
+    choices: ["blue", "orange", "red", "green"],
+    correctAnswer: 3
+  }, {
+    question: "What is the name of the kids show that features big purple dinosaur?",
+    choices: ["Thomas", "Fred", "Barney", "The Wiggles"],
+    correctAnswer: 2
+  }, {
+    question: "Who is condisered to be the greatest basketball player of all time?",
+    choices: ["Steph Curry", "Michael Jordan", "Larry Bird", "Kobe Bryant"],
+    correctAnswer: 1
+  }, {
+    question: "What is the capital of the United States of America?",
+    choices: ["Washington, D.C.", "Seattle", "Maryland", "Canada"],
+    correctAnswer: 0
+  }];
+
+  var mediumQuestions = [{
+    question: "What is James Bond's secret agent number?",
+    choices: ["911", "80", "4", "007"],
+    correctAnswer: 3
+  }, {
+    question: "How many edges does a cube have?",
+    choices: ["6", "1", "5", "10"],
+    correctAnswer: 0
+  }, {
+    question: "What is the biggest continent in the world?",
+    choices: ["Europe", "Asia", "Canada", "Alaska"],
+    correctAnswer: 1
+  }, {
+    question: "Which comic book superhero does not have any 'special powers'?",
+    choices: ["Superman", "Flash", "Batman", "Pinocchio"],
+    correctAnswer: 2
+  }, {
+    question: "Who is usually credited for the invention of the light bulb?",
+    choices: ["Thomas Edison", "Albert Einstein", "Geroge Washington", "Isaac Newton"],
+    correctAnswer: 0
+  }];
+
+  var hardQuestions = [{
+    question: "What does the 'www' stand for in 'www.google.com'?",
+    choices: ["webpage", "world wide web", "whiskey while working", "internet"],
+    correctAnswer: 1
+  }, {
+    question: "What is the square root of the number 25?",
+    choices: ["1", "7.43", "625", "5"],
+    correctAnswer: 3
+  }, {
+    question: "What American icon wore a 'coonskin cap?",
+    choices: ["Henry Ford", "Paul Bunyan", "Davy Crockett", "John Adams"],
+    correctAnswer: 2
+  }, {
+    question: "What kind of car did the 'Dukes of Hazard' drive",
+    choices: ["Dodge Charger", "Dodge Neon", "Chevrolet Camaro", "Ford Thunderbird"],
+    correctAnswer: 0
+  }, {
+    question: "What kind of animal was Lassie",
+    choices: ["elephant", "horse", "dolphin", "dog"],
+    correctAnswer: 3
+  }];
+
+  var diff = 0; //difficulty
+  var diffButtons = document.getElementsByName("diff");
+  var answerButtons = document.getElementsByName("answer");
+  var start = document.getElementById("start");
+    var previousNumber = 6;
+    var randomNum;
+
+  function getDifficulty() {
+      if (diffButtons[0].checked) {
+          diff = 1;
+      } else if (diffButtons[1].checked) {
+          diff = 2;
+      } else if (diffButtons[2].checked) {
+          diff = 3;
+      }
+  }
+  
+    function startQuiz() {
+        randomNumber();
+        if (randomNum === previousNumber) {
+            randomNumber();
+        } else {
+            previousNumber = randomNum;
+        }
+        
+        if (diff === 1) {
+            question.textContent = easyQuestions[randomNum];
+        } else if (diff === 2) {
+            question.textContent = mediumQuestions[randomNum];
+        } else if (diff === 3) {
+            question.textContent = hardQuestion[randomNum];
+        }
+        
+    }
+    
+    function randomNumber() {
+        randomNum = Math.floor(Math.random() * 5);
+    }
+  
 })
